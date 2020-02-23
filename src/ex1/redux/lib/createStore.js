d869958d6f669ff43c38837ce53d423ce1b5ef28
Dan Kreiger <dan@dankreiger.com>
2020-02-23 23:51:02 +0100

    chore(ex): update exercise 1

--- src/ex1/redux/lib/createStore.js
+++ src/ex1/redux/lib/createStore.js
@@ -8,38 +8,36 @@ export const createStore = reducer => {
 
   // fill out the 1, 2, and 3 below
 
-  // ------------ getState -----------------
+  // 1. ------------ getState -----------------
   // exercise: fill in the getState function
   //
-  const getState = () => {}
+  const getState = () => {};
 
   // hint: this is a function that returns the current state
   // ---------------------------------------
 
-
-  // ---------- subscribe ------------------
+  // 2. ---------- subscribe ------------------
   // exercise: fill in the subscribe function
-  const subscribe = listener => {}
+  const subscribe = listener => {};
 
-  // hint: 
-  // - 1. needs to adds the passed listener to the array of listeners
+  // hints:
+  // - needs to adds the passed listener to the array of listeners
   //   (see `ex1/index.js` for the function that will listen to the store)
   // - bonus: should return an function that removes the listener from the array of listeners (serves as unsubscribe)
   // ---------------------------------------
 
   // 3. -------- dispatch -----------------
   // exercise: fill in the dispatch function
-  const dispatch = action => {}
+  const dispatch = action => {};
 
-  // hints: 
-  // - 1. should assign the current state to the state returned by the reducer 
+  // hints:
+  // - should assign the current state to the state returned by the reducer
   //      that was passed into createStore
-  //      note: remember that the reducer is a function that accepts the current state and an action 
-  // - 2. dispatch should call every listener in the array of listeners after assigning the new current state
-
+  //      note: remember that the reducer is a function that accepts the current state and an action
+  // - dispatch should call every listener in the array of listeners after assigning the new current state
 
   // initialize state
   dispatch({});
 
-  return { getState, subscribe, dispatch }
-}
+  return { getState, subscribe, dispatch };
+};
