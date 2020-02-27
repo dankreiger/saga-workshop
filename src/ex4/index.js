68d02c3087c96ee88c0014eda3c8f887b98213fd
Dan Kreiger <dan@dankreiger.com>
2020-02-27 13:35:55 +0100

    chore: update workshop files

--- src/ex4/index.js
+++ src/ex4/index.js
@@ -8,8 +8,11 @@ const quoteUrl = 'https://api.quotable.io/random';
 // async generator example
 function* createQuoteFetcher() {
   // yields to fetch the data from the given url using the window fetch api
+  // ...
   // yields to get the json from the response (needs to be yielded since getting the json is also asynchronous 🤔)
+  // ...
   // returns the response (it should be in response.content for this api)
+  // ...
 }
 
 document.getElementById('facepalm').addEventListener('click', async () => {
